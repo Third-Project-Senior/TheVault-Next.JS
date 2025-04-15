a7e39628d0459876783e91c757e684f6697c07cc
Louay-Trabelsi <louayt250@gmail.com>
2025-04-15 11:54:09 +0100

    fixed comments

--- the-vault/src/components/Comments.jsx
+++ the-vault/src/components/Comments.jsx
@@ -1,15 +1,23 @@
+'use client'
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {jwtDecode} from 'jwt-decode';
+import {useParams} from 'next/navigation'
+
+
 
 const Comments = ({ productId }) => {
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState('');
   const [loading, setLoading] = useState(true);
 
+  const token = localStorage.getItem('token');
+
+
   useEffect(() => {
     const fetchComments = async () => {
       try {
-        const response = await axios.get(`http://localhost:3000/api/comments/${productId}`);
+        const response = await axios.get(`http://localhost:3000/api/comment/${productId}`);
         setComments(response.data);
       } catch (error) {
         console.error('Error fetching comments:', error);
@@ -23,11 +31,10 @@ const Comments = ({ productId }) => {
 
   const handleAddComment = async () => {
     try {
-      const token = localStorage.getItem('token');
-      const userId = jwtDecode(token).id;
+      const userId = jwtDecode(token).id ;
 
       const response = await axios.post(
-        'http://localhost:3000/api/comments',
+        'http://localhost:3000/api/comment',
         { content: newComment, userId, productId },
         { headers: { Authorization: `Bearer ${token}` } }
       );
